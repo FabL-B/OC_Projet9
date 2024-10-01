90322ef5d0bd8cea00f1b567a35b3e9ea89dee34
FabL-B <errebelneibaf@gmail.com>
2024-10-01 12:02:44 +0200

    Added image handling for tickets

--- LITReview/settings.py
+++ LITReview/settings.py
@@ -133,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = '/tickets/'
 LOGOUT_REDIRECT_URL = 'accounts:index'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
