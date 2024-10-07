6c2ec669f4bbfce802eb85a2595d641c1400cf7e
FabL-B <errebelneibaf@gmail.com>
2024-10-07 11:20:14 +0200

    add non-followed user reviews on connected user's tickets in flux and refactor flux

--- flux/urls.py
+++ flux/urls.py
@@ -4,13 +4,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'flux'
+app_name = "flux"
 urlpatterns = [
     # Page that shows all tickets and reviews from user and follow users(Flux)
     path("", views.flux, name="flux"),
     # Page that shows only tickets and reviews from user
-    path("posts/", views.posts, name='posts'),
+    path("posts/", views.posts, name="posts"),
     # Page that shows posts from a specific user
-    path("posts/<int:user_id>/", views.posts, name='user_posts'),
+    path("posts/<int:user_id>/", views.posts, name="user_posts"),
 ]
-
--- flux/views.py
+++ flux/views.py
@@ -12,15 +12,14 @@ from accounts.models import CustomUser
 @login_required
 def flux(request):
     """Show reviews and tickets from connected user and followed users."""
-    
-    # Get followed users
-    followed_users = UserFollows.objects.filter(user=request.user).values_list('followed_user', flat=True)
+
     # Get reviews from connected and followed users
-    reviews = Review.objects.filter(user__in=[request.user.id] + list(followed_users))
-    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
+    reviews = get_user_viewable_reviews(request.user)
+    reviews = reviews.annotate(content_type=Value("REVIEW", CharField()))
+
     # Get tickets from connected and followed users
-    tickets = Ticket.objects.filter(user__in=[request.user.id] + list(followed_users))
-    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
+    tickets = get_user_viewable_tickets(request.user)
+    tickets = tickets.annotate(content_type=Value("TICKET", CharField()))
 
     # Check if each ticket has an associated review
     for ticket in tickets:
@@ -33,8 +32,45 @@ def flux(request):
         reverse=True
     )
 
-    context = {'posts': posts}
-    return render(request, 'flux/flux.html', context)
+    context = {"posts": posts}
+    return render(request, "flux/flux.html", context)
+
+
+def get_user_viewable_reviews(user):
+    """Get a queryset of reviews viewable by connected user"""
+
+    # Get followed users
+    followed_users = UserFollows.objects.filter(
+        user=user).values_list('followed_user', flat=True)
+
+    # Get reviews from followed users and the connected user
+    reviews_from_followed = Review.objects.filter(
+        user__in=[user.id] + list(followed_users))
+
+    # Get reviews on tickets created by the connected user
+    reviews_on_user_tickets = Review.objects.filter(ticket__user=user)
+
+    # Combine reviews into a single queryset using union
+    viewable_reviews = reviews_from_followed | reviews_on_user_tickets
+    # Remove duplicates from reviews_on_user_tickets
+    viewable_reviews = viewable_reviews.distinct()
+
+    return viewable_reviews
+
+
+def get_user_viewable_tickets(user):
+    """Get a queryset of tickets viewable by connected user"""
+
+    # Get followed users
+    followed_users = UserFollows.objects.filter(
+        user=user).values_list('followed_user', flat=True)
+
+    # Get tickets from followed users and the connected user
+    viewable_tickets = Ticket.objects.filter(
+        user__in=[user.id] + list(followed_users))
+
+    return viewable_tickets
+
 
 @login_required
 def posts(request, user_id=None):
@@ -44,21 +80,24 @@ def posts(request, user_id=None):
         user = CustomUser.objects.get(id=user_id)
     else:
         user = request.user
-        
+
     # Get reviews from connected and followed users
     user_reviews = Review.objects.filter(user=user)
-    user_reviews = user_reviews.annotate(content_type=Value('REVIEW', CharField()))
+    user_reviews = user_reviews.annotate(
+        content_type=Value("REVIEW", CharField()))
     # Get tickets from connected and followed users
     user_tickets = Ticket.objects.filter(user=user)
-    user_tickets = user_tickets.annotate(content_type=Value('TICKET', CharField()))
-    
+    user_tickets = user_tickets.annotate(
+        content_type=Value("TICKET", CharField()))
+
     posts = sorted(
         chain(user_reviews, user_tickets),
         key=lambda post: post.time_created,
-        reverse=True
+        reverse=True,
     )
-    
-    context = {'posts': posts,
-               'user_profile': user,
-               }
-    return render(request, 'flux/posts.html', context)
+
+    context = {
+        "posts": posts,
+        "user_profile": user,
+    }
+    return render(request, "flux/posts.html", context)
